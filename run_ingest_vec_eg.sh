a46f7b15cc773c0c45ea074875fd45d42d21d56a
William Campbell <wcampbell@ll.mit.edu>
2016-01-12 15:38:02 -0500

    minor mod to example

--- run_ingest_vec_eg.sh
+++ run_ingest_vec_eg.sh
@@ -8,6 +8,10 @@ list=lists/list_test.txt
 precision=float
 outfile=datastore/test_ds.dat
 
+if [ ! -d datastore ] ; then
+	 mkdir datastore
+fi
+
 ./run_ingest_vec.py --precision $precision --list $list --outfile $outfile --num_bits 8
 ./run_ingest_vec.py --precision $precision --list $list --outfile $outfile --num_bits 16
 ./run_ingest_vec.py --precision $precision --list $list --outfile $outfile --num_bits 32
